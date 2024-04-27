93f426b765c3099e46890301a42aef00b90a21e3
akyarv0 <akyarv0@gmail.com>
2024-04-27 13:02:21 +0300

    navbar

--- mui-starter/src/pages/Home.jsx
+++ mui-starter/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import { CssBaseline } from "@mui/material";
+import Navbar from "../components/Navbar";
+import TextField from "../components/TextField";
 import TypoButtons from "../components/TypoButtons";
 
 const Home = () => {
   return (
     <>
+    <CssBaseline />
+      <Navbar />
       <TypoButtons />
+      <TextField />
     </>
   );
 };
