8a0508bf1f7287d274bc4a5d2def2b7de42b9af5
akyarv0 <akyarv0@gmail.com>
2024-04-27 11:45:56 +0300

    first commit

---
+++ mui-starter/src/components/TypoButtons.jsx
@@ -0,0 +1,13 @@
+// import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+const TypoButtons = () => {
+  return (
+    <div>
+      {" "}
+      <Typography variant="h1" component="h3">Deneme</Typography>
+    </div>
+  );
+};
+
+export default TypoButtons;
